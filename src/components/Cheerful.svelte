0966640153f4bd0cfcc2b45ea56c79e1dc2396f3
Mikael Brevik <mikaelbre@gmail.com>
2019-09-30 22:34:14 +0200

    Adds some minor improvements to the design

--- src/components/Cheerful.svelte
+++ src/components/Cheerful.svelte
@@ -5,23 +5,26 @@
 
   export let username;
 
-  let data = {};
   let isLoading = true;
-  let error;
+  let data;
+  let error = "";
 
   onMount(async function() {
     try {
       const res = await fetch(`api/${username}.json`);
       const json = await res.json();
 
+      if (res.status >= 300) {
+        throw new Error(json.message);
+      }
+
       data = json;
-      console.log(data);
       if (isOneFaceHappy(data.faces)) {
         document.title = data.user.name + " looks cheerful";
       }
     } catch (e) {
       console.error("Error: ", e);
-      error = e.message;
+      error = e;
     } finally {
       isLoading = false;
     }
@@ -45,11 +48,46 @@
     display: flex;
     flex-direction: column;
     justify-content: center;
+
+    position: relative;
+    z-index: 10;
+  }
+  .githubContent__metadata::before {
+    content: "";
+    display: block;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: -5rem;
+    width: 100%;
+    z-index: -1;
+
+    background: url("/separator.svg") right center no-repeat;
+  }
+
+  .githubContent__avatar {
+    flex: 1;
   }
   .githubContent__avatar img {
     display: block;
+    max-width: 100%;
+    max-height: 100%;
   }
 
+  .githubContent__follow {
+    font-size: 0.8rem;
+    font-style: italic;
+
+    margin-top: 0.5rem;
+  }
+
+  .githubContent__metadata h1 {
+    color: var(--color__primaryText);
+    font-size: 2rem;
+    font-weight: bold;
+    text-align: center;
+    display: block;
+  }
   .githubContent__metadata h1 a {
     color: var(--color__primaryText);
     transition: all 500ms ease-in;
@@ -68,20 +106,39 @@
   <div class="contentBox">
     <article class="contentBox__inner githubContent">
       {#if isLoading}
-        <p>Loading</p>
-      {:else if error}
-        <p>Error: {error}</p>
-      {:else}
+        <div class="githubContent__metadata">
+          <h1>Loading...</h1>
+          <img src="/loading.svg" alt="Loading" />
+        </div>
+        <figure class="githubContent__avatar">
+          <img src="/duck.jpg" alt="Loading duck" />
+        </figure>
+      {:else if data}
         <div class="githubContent__metadata">
           <h1>
-            <a href={data.user.html_url}>{data.user.name}</a>
+            <a href={data.user.html_url}>{data.user.name || data.user.login}</a>
           </h1>
           <Happy faces={data.faces} />
+
+          <p class="githubContent__follow">
+            They have {data.user.followers} people following them, why don't you
+            <a href={data.user.html_url}>increase that?</a>
+          </p>
         </div>
         <figure class="githubContent__avatar">
           <a href={data.user.html_url}>
-            <img src={data.user.avatar_url} alt={data.user.name} />
+            <img
+              src={data.user.avatar_url}
+              alt={data.user.name || data.user.login} />
           </a>
+        </figure>
+      {:else}
+        <div class="githubContent__metadata">
+          <h1>Oh no...</h1>
+          <p>Failed duck {error}</p>
+        </div>
+        <figure class="githubContent__avatar">
+          <img src="/duck.jpg" alt="Loading duck" />
         </figure>
       {/if}
     </article>
--- src/routes/api/_api.js
+++ src/routes/api/_api.js
@@ -6,6 +6,11 @@ const baseUrl = "https://api.github.com";
 export async function getUser(username) {
   let result = await fetch(`${baseUrl}/users/${username}`);
   let data = await result.json();
+
+  if (result.status >= 300) {
+    throw new Error(data.message);
+  }
+
   const image = data.avatar_url;
   const faces = await getEmotion(image);
   return { user: data, faces };
