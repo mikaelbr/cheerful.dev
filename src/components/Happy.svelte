db8df52cd8c9805fdc491046a2e15ad14ba13ecc
Mikael Brevik <mikaelbre@gmail.com>
2019-10-01 08:42:43 +0200

    Adds emoji illustrating cheerfulness

--- src/components/Happy.svelte
+++ src/components/Happy.svelte
@@ -1,23 +1,51 @@
 <script>
   export let faces;
-  import { isHappy, isOneFaceHappy } from "./_utils";
+  import { isHappy, isOneFaceHappy, getMaxHappiness } from "./_utils";
+
+  function happinessToEmoji(factor) {
+    if (factor > 0.2) {
+      return "😀";
+    }
+    if (factor > 0.4) {
+      return "😃";
+    }
+    if (factor > 0.6) {
+      return "😄";
+    }
+    if (factor > 0.8) {
+      return "😁";
+    }
+    if (factor >= 1) {
+      return "😆";
+    }
+    return "😐";
+  }
+
+  let emoji = happinessToEmoji(getMaxHappiness(faces));
 </script>
 
 <style>
+  .emoji {
+    font-size: 3rem;
+    text-align: center;
+  }
 
+  .emotionText {
+    font-style: italic;
+    font-size: 0.9rem;
+  }
 </style>
 
-{#if faces.length === 0}
-  <p>
+<p class="emoji">{emoji}</p>
+<p class="emotionText">
+  {#if faces.length === 0}
     I couldn't find a face, so I honestly don't know if they are cheerful or
     not.
-  </p>
-{:else if faces.length > 1 && isOneFaceHappy(faces)}
-  <p>... looks like several people, but atleast one of them are cheerful! 😄</p>
-{:else if faces.length > 1 && !isOneFaceHappy(faces)}
-  <p>... looks like several people, and none of them look really cheerful.</p>
-{:else if faces.length && isHappy(faces[0])}
-  <p>...looks like one cheerful developer! 😁</p>
-{:else}
-  <p>... doesn't look like the most cheerful developer.</p>
-{/if}
+  {:else if faces.length > 1 && isOneFaceHappy(faces)}
+    ... looks like several people, but atleast one of them are cheerful!
+  {:else if faces.length > 1 && !isOneFaceHappy(faces)}
+    ... looks like several people, and none of them look really cheerful.
+  {:else if faces.length && isHappy(faces[0])}
+    ...looks like one cheerful developer!
+  {:else}... doesn't look like the most cheerful developer.{/if}
+</p>
--- src/components/_utils.js
+++ src/components/_utils.js
@@ -1,9 +1,17 @@
 const happynessFactor = 0.2;
 
+function getHappy(face) {
+  return face.faceAttributes.emotion.happiness;
+}
+
 export function isOneFaceHappy(faces) {
   return faces.some(isHappy);
 }
 
 export function isHappy(face) {
   return face.faceAttributes.emotion.happiness >= happynessFactor;
+}
+
+export function getMaxHappiness(faces) {
+  return Math.max(0, ...faces.map(getHappy));
 }
